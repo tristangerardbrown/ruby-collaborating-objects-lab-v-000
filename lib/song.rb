cb400f83ec8f7232a65f22f51ccd20a3d67f394b
Tristan Brown <tgbrown@stanford.edu>
2018-09-26 09:58:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,16 +6,18 @@ class Song
     @name = name
   end
 
-  def artist_name=(name_string)
-       song_artist = Artist.find_or_create_by_name(name_string)
-        @artist = song_artist
-       @artist.add_song(self)
-   end
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.find_or_create_by_name(name)
+    else
+      self.artist_name = name
+    end
+  end
 
   def self.new_by_filename(filename)
       song_name = filename.split(" - ")[1]
       new_song = self.new(song_name)
-      new_song.artist = filename.split(" - ")[0]
+      new_song.artist_name = filename.split(" - ")[0]
     new_song
   end
 
