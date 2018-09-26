d324be64a10fb270680e83e58d87d24601fe34bb
Tristan Brown <tgbrown@stanford.edu>
2018-09-26 10:11:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,25 +1,19 @@
 class Song
-
-  attr_accessor :name, :artist
-
-  def initialize(name)
-    @name = name
+ 	attr_accessor :name, :artist
+ 	def initialize(name)
+		@name = name
+	end
+ 	def artist_name=(name)
+	  if (self.artist.nil?)
+		  self.artist = Artist.find_or_create_by_name(name)
+	  else
+		  self.artist.name = name
+	  end
   end
-
-  def artist_name=(artist_name)
-    if (self.artist.nil?)
-      self.artist = Artist.find_or_create_by_name(name)
-    else
-      self.artist.name = artist_name
-    end
+ 	def self.new_by_filename(filename)
+	  song_name = filename.split(" - ")[1]
+	  new_song = self.new(song_name)
+	  new_song.artist_name = filename.split(" - ")[0]
+  	new_song
   end
-
-  def self.new_by_filename(filename)
-      song_name = filename.split(" - ")[1]
-      new_song = self.new(song_name)
-      new_song.artist_name = filename.split(" - ")[0]
-      new_song
-  end
-
-
-end
+ end
