1e7dc9280ad28e08bdc6ac0b62556b38bcbf081f
Tristan Brown <tgbrown@stanford.edu>
2018-09-26 09:22:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
   def self.new_by_filename(filename)
       song_name = filename.split(" - ")[1]
       new_song = self.new(song_name)
-      new_song.artist_name = filename.split(" - ")[0]
+      new_song.artist = filename.split(" - ")[0]
     new_song
   end
 
